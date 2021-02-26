949cfe1e77613049b8afbe0b1417dfbe0e3106d5
sdk-team <sdk-team@alibabacloud.com>
2021-02-26 13:26:53 +0000

    Generated 2018-07-13 for Ft.

--- services/ft/ft_ip_flow_control.go
+++ services/ft/ft_ip_flow_control.go
@@ -71,14 +71,17 @@ func (client *Client) FtIpFlowControlWithCallback(request *FtIpFlowControlReques
 // FtIpFlowControlRequest is the request struct for api FtIpFlowControl
 type FtIpFlowControlRequest struct {
 	*requests.RpcRequest
-	Name string `position:"Query" name:"Name"`
+	StringList string `position:"Query" name:"StringList"`
 }
 
 // FtIpFlowControlResponse is the response struct for api FtIpFlowControl
 type FtIpFlowControlResponse struct {
 	*responses.BaseResponse
-	RequestId string `json:"RequestId" xml:"RequestId"`
-	Name      string `json:"Name" xml:"Name"`
+	RequestId    string                 `json:"RequestId" xml:"RequestId"`
+	Names        NamesInFtIpFlowControl `json:"Names" xml:"Names"`
+	Names01      Names01                `json:"Names01" xml:"Names01"`
+	IntegerList  IntegerList            `json:"IntegerList" xml:"IntegerList"`
+	IntegerList1 IntegerList1           `json:"IntegerList1" xml:"IntegerList1"`
 }
 
 // CreateFtIpFlowControlRequest creates a request to invoke FtIpFlowControl API
